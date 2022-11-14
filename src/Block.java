22ebb69b723b7b77c28099244ba43d12aa82a9d4
Seanluc430 <seanlucibello@gmail.com>
2022-11-13 20:43:05 -0500

    Added Block class

---
+++ src/Block.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String prevHash;
+    private String data;
+    private long timeStamp;
+
+    //Constructor
+    public Block(String data, String prevHash){
+        this.data = data;
+        this.prevHash = prevHash;
+        timeStamp = new Date().getTime();
+    }
+}
